5a2eac269cb097085f947d192804e1261ce2e2e7
MahmutEngin <155198520+MahmutEngin@users.noreply.github.com>
2025-05-21 23:12:22 +0200

    Create Mahmut_Engin_TL_week6.py

---
+++ Mahmut_Engin_TL_week6.py
@@ -0,0 +1,17 @@
+class TaskManagement():
+    task_list =[]
+    def gorev_ekle():
+        pass
+class PersonalTask (Task):
+    pass
+class WorkTask(Task):
+    pass
+
+class TaskEditing():
+    pass
+
+class TaskTracking():
+    pass
+
+class Task:
+    pass
