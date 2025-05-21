74bfbb4a5db3b16a30c77a92fa0bc793f47e74e4
MahmutEngin <155198520+MahmutEngin@users.noreply.github.com>
2025-05-21 23:44:38 +0200

    Update Mahmut_Engin_TL_week6.py

--- Mahmut_Engin_TL_week6.py
+++ Mahmut_Engin_TL_week6.py
@@ -1,17 +1,50 @@
+class Task:
+    def __init__(self,task_id,task_name, deadline,status,priority,color):
+        self.task_id=task_id
+        self.task_name=task_name
+        self.deadline=deadline
+        self.status=status
+        self.priority=priority
+        self.color=color
+    def days_until_deadline():
+        pass
+    def color_your_task():
+        pass
 class TaskManagement():
     task_list =[]
-    def gorev_ekle():
+    def add_task(task:Task):
+        pass
+    def list_tasks():
         pass
 class PersonalTask (Task):
+    def __init__(self, task_id, task_name, deadline, status, priority, color):
+        super().__init__(task_id, task_name, deadline, status, priority, color)
     pass
 class WorkTask(Task):
+    def __init__(self, task_id, task_name, deadline, status, priority, color):
+        super().__init__(task_id, task_name, deadline, status, priority, color)
     pass
 
 class TaskEditing():
-    pass
-
+    def find_task_by_id(task_id):
+        pass
+    def update_status(task_id, new_status): 
+        pass
+    def update_priority(task_id, new_priority): 
+        pass
+    def update_deadline(task_id, new_deadline): 
+        pass
+   
 class TaskTracking():
-    pass
 
-class Task:
-    pass
+    def get_status(task_id):
+        pass
+    def get_deadline(task_id): 
+        pass
+    def get_color(task_id): 
+        pass
+
+
+
+       
+  
